314d817440fcf7e0227d7ae7b9c06d60507aa04e
LodgeFab <lodgefab@gmail.com>
2022-04-09 07:16:11 +0900

    add exit before enter

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -21,7 +21,7 @@ const Layout = ({ children}: Props) => {
       /> */}
         <Menu width={MenuWidth}/>
         
-        <AnimatePresence>
+        <AnimatePresence exitBeforeEnter>
           
             {children}
           
