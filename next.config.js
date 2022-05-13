06e7635fca7c248d05c370411a83e2d7de3a2c11
LodgeFab <lodgefab@gmail.com>
2022-05-13 12:17:28 +0900

    fix auth setting

--- next.config.js
+++ next.config.js
@@ -8,4 +8,9 @@ module.exports = {
   images: {
     domains: ['s3.us-west-2.amazonaws.com'],
   },
+  env: {
+    ENABLE_BASIC_AUTH: process.env.ENABLE_BASIC_AUTH,
+    BASIC_AUTH_USER: process.env.BASIC_AUTH_USER,
+    BASIC_AUTH_PASSWORD: process.env.BASIC_AUTH_PASSWORD,
+  },
 }
--- src/pages/_middleware.ts
+++ src/pages/_middleware.ts
@@ -1,16 +1,17 @@
 import { NextRequest, NextResponse } from 'next/server'
 
 export const middleware = (req: NextRequest) => {
+  if (process.env.ENABLE_BASIC_AUTH !== 'true') {
+    return NextResponse.next()
+  }
+
   const basicAuth = req.headers.get('authorization')
 
   if (basicAuth) {
     const auth = basicAuth.split(' ')[1]
     const [user, password] = Buffer.from(auth, 'base64').toString().split(':')
 
-    if (
-      user === process.env.BASIC_AUTH_USER &&
-      password === process.env.BASIC_AUTH_PASSWORD
-    ) {
+    if (user === process.env.BASIC_AUTH_USER && password === process.env.BASIC_AUTH_PASSWORD) {
       return NextResponse.next()
     }
   }
