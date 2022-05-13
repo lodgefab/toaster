405dd9c8249c4ecc5f66762aebb7e9cc3d39b02f
LodgeFab <lodgefab@gmail.com>
2022-05-13 15:43:21 +0900

    [WIP]change glb

--- src/components/molecules/HeroView.tsx
+++ src/components/molecules/HeroView.tsx
@@ -26,48 +26,55 @@ import { MotionConfig } from "framer-motion";
 
 type Props = {
   model: string;
-  isReady:boolean
+  isReady: boolean;
 };
 
 const Loader = () => {
-  const { active, progress, errors, item, loaded, total } = useProgress()
-  return <Html center>{progress} % loaded</Html>
+  const { active, progress, errors, item, loaded, total } = useProgress();
+  return <Html center>{progress} % loaded</Html>;
 };
 
 const HeroView: React.VFC<Props> = ({ model, isReady }) => {
   const Model = lazy(() => import(`../../../public/models/${model}.tsx`));
 
   return (
-      <Canvas
-        camera={{ position: [0, 40, 80], fov: 80 }}
-        orthographic={true}
-        dpr={[1, 2]}
-      >
-        {/* <Zoom/> */}
-        <ambientLight />
-        <hemisphereLight
-          color="#eeeeee"
-          groundColor="#eeeeee"
-          position={[-7, 25, 13]}
-          intensity={1}
-        />
-        <Suspense fallback={<Loader />}>
-          <PresentationControls
-            global
-            rotation={[0, -Math.PI / 4, 0]}
-            polar={[0, Math.PI / 4]}
-            azimuth={[-Math.PI / 4, Math.PI / 4]}
-            cursor={false} 
-            snap={true}
-          >
-            <Bounds fit clip margin={1.2}>
-              <Model isReady={isReady}/>
-            </Bounds>
-            <gridHelper args={[10, 40, '#aaaaaa', '#cccccc']} position={[-0.25, -1.2, 0]} rotation={[0, 0, 0]} />
-          </PresentationControls>
-        </Suspense>
-        {/* <OrthographicCamera zoom={1}/> */}
-      </Canvas>
+    <Canvas
+      camera={{ position: [0, 40, 80], fov: 80 }}
+      orthographic={true}
+      dpr={[1, 2]}
+    >
+      {/* <Zoom/> */}
+      <ambientLight 
+        color='#ffffff'
+        intensity={1}
+      />
+      <hemisphereLight
+        color="#eeeeee"
+        groundColor="#eeeeee"
+        position={[-7, 25, 13]}
+        intensity={1}
+      />
+      <Suspense fallback={<Loader />}>
+        <PresentationControls
+          global
+          rotation={[0, -Math.PI / 4, 0]}
+          polar={[0, Math.PI / 4]}
+          azimuth={[-Math.PI / 4, Math.PI / 4]}
+          cursor={false}
+          snap={true}
+        >
+          <Bounds fit clip margin={1.2}>
+            <Model isReady={isReady} />
+          </Bounds>
+          <gridHelper
+            args={[10, 40, "#aaaaaa", "#cccccc"]}
+            position={[-0.25, -1.2, 0]}
+            rotation={[0, 0, 0]}
+          />
+        </PresentationControls>
+      </Suspense>
+      {/* <OrthographicCamera zoom={1}/> */}
+    </Canvas>
   );
 };
 
