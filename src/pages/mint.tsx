a0c4cd350628a9dd07430587711b16af7bec7b24
LodgeFab <lodgefab@gmail.com>
2022-06-23 17:37:26 +0900

    use Alchemy RPC

--- src/pages/mint.tsx
+++ src/pages/mint.tsx
@@ -41,7 +41,7 @@ const Mint = ({}: InferGetStaticPropsType<typeof getServerSideProps>) => {
       </Head>
       <IframeWrap>
         <iframe
-          src="https://gateway.ipfscdn.io/ipfs/QmRPK2zjmkM8sodgCpyiCbLTvKGJwU3mWcwXwL2AWEBWym/token-drop.html?contract=0x5C8CA8C3A00C1Cd127018899Dd50D4C6E0174c2C&chainId=4&rpcUrl=https%3A%2F%2Fwww.toaster.design%2Ftoast.png&relayUrl=https%3A%2F%2Fapi.defender.openzeppelin.com%2Fautotasks%2F9bbfb5b4-478b-4686-aa2b-e4751c8cc361%2Fruns%2Fwebhook%2F38c8f056-c9f1-4f83-944e-b0cbd24ad3b8%2FAkqej5iqtiQjvToUPEs1BH"
+          src="https://gateway.ipfscdn.io/ipfs/QmRPK2zjmkM8sodgCpyiCbLTvKGJwU3mWcwXwL2AWEBWym/token-drop.html?contract=0x5C8CA8C3A00C1Cd127018899Dd50D4C6E0174c2C&chainId=4&rpcUrl=https%3A%2F%2Feth-rinkeby.alchemyapi.io%2Fv2%2FGVwDpP4it_Zzl2xItZh9DsXW1GGAHexY&relayUrl=https%3A%2F%2Fapi.defender.openzeppelin.com%2Fautotasks%2F9bbfb5b4-478b-4686-aa2b-e4751c8cc361%2Fruns%2Fwebhook%2F38c8f056-c9f1-4f83-944e-b0cbd24ad3b8%2FAkqej5iqtiQjvToUPEs1BH"
           width="600px"
           height="600px"
           frameBorder="0"
