e6f2b34b89ef1118dfc920638e44be694ba13aef
LodgeFab <lodgefab@gmail.com>
2022-06-23 16:30:45 +0900

    fix iframe width

--- src/pages/mint.tsx
+++ src/pages/mint.tsx
@@ -1,18 +1,9 @@
-import {
-  GetServerSideProps,
-  GetStaticProps,
-  InferGetStaticPropsType,
-} from "next";
+import { InferGetStaticPropsType } from "next";
 import Head from "next/head";
-import { BlogPost } from "../../@types/schema";
 import NotionService from "../services/notion-service";
-import RecipeCard from "../components/molecules/RecipeCard";
-import styled from "@emotion/styled";
-import Footer from "../components/molecules/Footer";
-import Layout from "../components/Layout";
-import { color, font } from "../styles";
-import { Home } from "../components/organisms/Home";
+
 import { motion } from "framer-motion";
+import styled from "@emotion/styled";
 
 export async function getServerSideProps() {
   const notionService = new NotionService();
@@ -48,14 +39,22 @@ const Mint = ({}: InferGetStaticPropsType<typeof getServerSideProps>) => {
           content={title}
         />
       </Head>
-      <iframe
-        src="https://gateway.ipfscdn.io/ipfs/QmRPK2zjmkM8sodgCpyiCbLTvKGJwU3mWcwXwL2AWEBWym/token-drop.html?contract=0x5C8CA8C3A00C1Cd127018899Dd50D4C6E0174c2C&chainId=4"
-        width="600px"
-        height="600px"
-        frameBorder="0"
-      ></iframe>
+      <IframeWrap>
+        <iframe
+          src="https://gateway.ipfscdn.io/ipfs/QmRPK2zjmkM8sodgCpyiCbLTvKGJwU3mWcwXwL2AWEBWym/token-drop.html?contract=0x5C8CA8C3A00C1Cd127018899Dd50D4C6E0174c2C&chainId=4"
+          width="600px"
+          height="600px"
+          frameBorder="0"
+        ></iframe>
+      </IframeWrap>
     </motion.div>
   );
 };
 
 export default Mint;
+
+const IframeWrap = styled.div`
+  iframe {
+    width: 100vw;
+  }
+`;
